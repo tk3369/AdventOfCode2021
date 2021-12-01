9df996694481b42da459784b16b778bf16da8269
Tom Kwong <tk3369@gmail.com>
2021-11-30 22:39:31 -0800

    Update

--- day01.jl
+++ day01.jl
@@ -1,18 +1,16 @@
 data() = parse.(Int, readlines("day01.txt"))
+depth = data()
 
-function part1()
-    depths = data()
+function part1(depths)
     return count(x -> x > 0, diff(depths))
 end
 # ans: 1292
 
-function part2()
-    depths = data()
+function part2(depths)
     sums = [sum(depths[i:i+2]) for i in 1:length(depths)-2]
     return count(x -> x > 0, diff(sums))
 end
 # ans: 1262
-
 
 using Plots
 
@@ -32,4 +30,14 @@ function overlay_graph()
         frame(anim)
     end
     gif(anim, "day01_anim.gif", fps = 15)
+end
+
+# This solution uses the fact that the sliding window comparison is
+# really just comparing the first and the 4th element:
+#     a1 + a2 + a3 < a2 + a3 + a4?
+# which is equivalent to answering a1 < a4
+function part2_jling(depths)
+    count(3:lastindex(depths)-1) do idx
+        depths[idx-2] < depths[idx+1]
+    end
 end
