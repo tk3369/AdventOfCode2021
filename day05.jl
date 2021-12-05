8b417d8781a1c03991c13ed40fa9538d600cc3a9
Tom Kwong <tk3369@gmail.com>
2021-12-04 23:49:43 -0800

    Day 5 lines image, reformatting code

--- day05.jl
+++ day05.jl
@@ -16,14 +16,16 @@ function parse_coordinates(coord)
 end
 
 "Returns true if the line is either horizontal or vertical"
-is_hv(line) = let (a, b) = line
-    a.x == b.x || a.y == b.y
-end
+is_hv(line) =
+    let (a, b) = line
+        a.x == b.x || a.y == b.y
+    end
 
 "Returns true if the line is diagonal"
-is_diag(line) = let (a, b) = line
-    abs(a.x - b.x) == abs(a.y - b.y)
-end
+is_diag(line) =
+    let (a, b) = line
+        abs(a.x - b.x) == abs(a.y - b.y)
+    end
 
 "Include the line only if it is horizontal, vertical, or diagonal at 45 degrees"
 is_valid(line) = is_hv(line) || is_diag(line)
@@ -38,12 +40,12 @@ end
 
 function run(input, filter_fn)
     lines = filter(filter_fn, input)
-    max_x = maximum(max(a.x, b.x) for (a,b) in lines)
-    max_y = maximum(max(a.y, b.y) for (a,b) in lines)
+    max_x = maximum(max(a.x, b.x) for (a, b) in lines)
+    max_y = maximum(max(a.y, b.y) for (a, b) in lines)
     # Use zero-based index easily!
     board = OffsetArray(zeros(Int, max_y + 1, max_x + 1), 0:max_y, 0:max_x)
     for (a, b) in lines
-        for (x,y) in zip(ranges(a, b)...)
+        for (x, y) in zip(ranges(a, b)...)
             board[y, x] += 1
         end
     end
@@ -66,15 +68,15 @@ using Plots
 function chart1()
     input = read_data("day05.txt")
     lines = filter(is_valid, input)
-    max_x = maximum(max(a.x, b.x) for (a,b) in lines)
-    max_y = maximum(max(a.y, b.y) for (a,b) in lines)
+    max_x = maximum(max(a.x, b.x) for (a, b) in lines)
+    max_y = maximum(max(a.y, b.y) for (a, b) in lines)
     board = OffsetArray(zeros(Int, max_y + 1, max_x + 1), 0:max_y, 0:max_x)
-    p = plot(; title = "Advent of Code 2021 - Day 5", legend = false,
-        size = (600, 600))
+    p = plot(; title = "Advent of Code 2021 - Day 5",
+        legend = false, size = (600, 600))
     for (a, b) in lines
-        p = plot!([a, b]; background=:black, foreground=:green,
-            guidefontsize=20, linewidth=5, alpha=0.2,
-            legend = false)
+        p = plot!([a, b]; background = :black, foreground = :green,
+            guidefontsize = 20, linewidth = 5, alpha = 0.2,
+            guides = "", showaxis = false, ticks = false)
     end
     savefig("day05_lines.png")
     return p
@@ -83,18 +85,18 @@ end
 function chart2()
     input = read_data("day05.txt")
     lines = filter(is_valid, input)
-    max_x = maximum(max(a.x, b.x) for (a,b) in lines)
-    max_y = maximum(max(a.y, b.y) for (a,b) in lines)
+    max_x = maximum(max(a.x, b.x) for (a, b) in lines)
+    max_y = maximum(max(a.y, b.y) for (a, b) in lines)
     board = zeros(Int, max_y + 1, max_x + 1)
     for (a, b) in lines
-        for (x,y) in zip(ranges(a, b)...)
+        for (x, y) in zip(ranges(a, b)...)
             board[y, x] += 1
         end
     end
     p = heatmap(board;
         title = "Advent of Code 2021 - Day 5",
         color = :thermal, legend = false, size = (600, 600),
-        background=:black, foreground=:green)
+        background = :black, foreground = :green)
     savefig("day05_heatmap.png")
     return p
 end
@@ -103,17 +105,17 @@ end
 function chart3()
     input = read_data("day05.txt")
     lines = filter(is_valid, input)
-    max_x = maximum(max(a.x, b.x) for (a,b) in lines)
-    max_y = maximum(max(a.y, b.y) for (a,b) in lines)
+    max_x = maximum(max(a.x, b.x) for (a, b) in lines)
+    max_y = maximum(max(a.y, b.y) for (a, b) in lines)
     board = zeros(Int, max_y + 1, max_x + 1)
     anim = Animation()
     plot(;
         title = "Advent of Code 2021 - Day 5",
         legend = false, size = (600, 600),
-        background=:black, foreground=:green);
+        background = :black, foreground = :green)
     frame(anim)
     for (i, (a, b)) in enumerate(lines)
-        for (x,y) in zip(ranges(a, b)...)
+        for (x, y) in zip(ranges(a, b)...)
             board[y, x] += 1
         end
         if i % 100 == 0
