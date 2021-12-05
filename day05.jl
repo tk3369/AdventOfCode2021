2fe1dc93ec399941019be3176c1cf765ffb26376
Tom Kwong <tk3369@gmail.com>
2021-12-04 22:20:41 -0800

    Refactored day 5 code

--- day05.jl
+++ day05.jl
@@ -1,91 +1,59 @@
+"""
+Read data and return array of lines. Each line is in this format.
+[(x = 645, y = 570), (x = 517, y = 570)]
+"""
 function read_data(filename)
     lines = readlines(filename)
-    coords = map.(xy, split.(lines, " -> "))
-    map(c -> (c[1], c[2]), coords)
+    return map.(parse_coordinates, split.(lines, " -> "))
 end
 
-function xy(coord)
+"Return a named tuple given a string of `x,y`"
+function parse_coordinates(coord)
     v = parse.(Int, split(coord, ","))
-    (x = v[1], y = v[2])
+    return (x = v[1], y = v[2])
 end
 
-hv(line) = line[1].x == line[2].x || line[1].y == line[2].y
+"Returns true if the line is either horizontal or vertical"
+is_hv(line) = let (a, b) = line
+    a.x == b.x || a.y == b.y
+end
 
-function is_diag(line)
-    a, b = line
+"Returns true if the line is diagonal"
+is_diag(line) = let (a, b) = line
     abs(a.x - b.x) == abs(a.y - b.y)
 end
 
-function diag_range(line)
-    a, b = line
-    xinc = b.x > a.x ? 1 : -1
-    yinc = b.y > a.y ? 1 : -1
-    return a.x:xinc:b.x, a.y:yinc:b.y
+"Include the line only if it is horizontal, vertical, or diagonal at 45 degrees"
+is_valid(line) = is_hv(line) || is_diag(line)
+
+"Return x & y coordinate iterator/ranges going from point `a` to `b`"
+function ranges(a, b)
+    Δ(s, t) = s < t ? 1 : -1
+    xr = a.x == b.x ? Iterators.repeated(a.x, abs(a.y - b.y) + 1) : a.x:Δ(a.x, b.x):b.x
+    yr = a.y == b.y ? Iterators.repeated(a.y, abs(a.x - b.x) + 1) : a.y:Δ(a.y, b.y):b.y
+    return xr, yr
 end
 
-function part1(input)
-    lines = filter(hv, input)
-    max_x = max(
-        maximum(getproperty.(getindex.(lines, 1), :x)),
-        maximum(getproperty.(getindex.(lines, 2), :x))
-    )
-    max_y = max(
-        maximum(getproperty.(getindex.(lines, 1), :y)),
-        maximum(getproperty.(getindex.(lines, 2), :y)),
-    )
-    @show max_x max_y
+function run(input, filter_fn)
+    lines = filter(filter_fn, input)
+    max_x = maximum(max(a.x, b.x) for (a,b) in data)
+    max_y = maximum(max(a.y, b.y) for (a,b) in data)
     M = zeros(Int, max_y + 1, max_x + 1)  # zero based coordinates
-    for line in lines
-        if line[1].x == line[2].x  # vertical
-            y1 = min(line[1].y, line[2].y)+1
-            y2 = max(line[1].y, line[2].y)+1
-            M[y1:y2, line[1].x+1] .+= 1
-        else  # y coordinates must equal, horizontal
-            x1 = min(line[1].x, line[2].x)+1
-            x2 = max(line[1].x, line[2].x)+1
-            M[line[1].y+1, x1:x2] .+= 1
+    for (a, b) in lines
+        xr, yr = ranges(a, b)
+        for (x,y) in zip(collect(xr), collect(yr))
+            M[y+1, x+1] += 1
         end
     end
-    return count(>(1), M) #, M
-end
-
-function part2(input)
-    lines = input #filter(hv, input)
-    max_x = max(
-        maximum(getproperty.(getindex.(lines, 1), :x)),
-        maximum(getproperty.(getindex.(lines, 2), :x))
-    )
-    max_y = max(
-        maximum(getproperty.(getindex.(lines, 1), :y)),
-        maximum(getproperty.(getindex.(lines, 2), :y)),
-    )
-    # @show max_x max_y
-    M = zeros(Int, max_y + 1, max_x + 1)  # zero based coordinates
-    for line in lines
-        a, b = line
-        if a.x == b.x  # vertical
-            y1 = min(a.y, b.y)+1
-            y2 = max(a.y, b.y)+1
-            M[y1:y2, a.x+1] .+= 1
-        elseif a.y == b.y # y coordinates must equal, horizontal
-            x1 = min(a.x, b.x)+1
-            x2 = max(a.x, b.x)+1
-            M[a.y+1, x1:x2] .+= 1
-        else
-            @show r = diag_range(line)
-            for (x,y) in zip(collect(r[1]), collect(r[2]))
-                M[y+1, x+1] += 1
-            end
-        end
-    end
-    return count(>(1), M) #, M
+    return count(>(1), M)
 end
 
 #=
-data = read_data("day05_sample.txt")
-part1(data)
-part2(data)
+sample_data = read_data("day05_sample.txt")
+run(sample_data, is_hv)
+run(sample_data, is_valid)
 
 data = read_data("day05.txt")
-part1(data)
+run(data, is_hv)         # 4421
+run(data, is_valid)      # 18674
 =#
