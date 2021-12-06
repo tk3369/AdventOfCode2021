4b1cb3ded590a0407673f4fa4877b95eee389160
Tom Kwong <tk3369@gmail.com>
2021-12-05 20:56:24 -0800

    Add insipirations

--- day05.jl
+++ day05.jl
@@ -126,3 +126,33 @@ function chart3()
     end
     gif(anim, "day05_anim.png"; fps = 5)
 end
+
+# Inspirations
+
+# Adapted from Jling@Zuplip
+# - CartesianIndex has a convenient diag() function
+using LinearAlgebra
+
+function solve_ci(filename)
+    CI = CartesianIndex
+    _sign(x1,x2) = x1>=x2 ? -1 : 1
+
+    lines = split.(readlines(filename), r",| -> ")
+    coords = map(lines) do line
+        c = parse.(Int, line)
+        step = CI(_sign(c[1], c[3]), _sign(c[2], c[4]))
+        CI(c[1], c[2]):step:CI(c[3], c[4])
+    end
+    # part 1
+    M = zeros(Int, 1000, 1000)
+    for c in coords
+        any(==(1), size(c)) && (M[c] .+= 1)
+    end
+    println("P1: ", sum(>=(2), M))
+    # part 2
+    for c in coords
+        any(==(1), size(c)) || (M[diag(c)] .+= 1)
+    end
+    println("P2: ", sum(>=(2), M))
+end
+
