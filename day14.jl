0002560d24554898ea2dfc5795e5a64044ef698b
Tom Kwong <tk3369@gmail.com>
2021-12-13 23:52:52 -0800

    Refactor code

--- day14.jl
+++ day14.jl
@@ -10,33 +10,30 @@ function read_data(filename)
 end
 
 function step!(dct, rules)
-    for (k, cnt) in collect(dct)  # must use collect here to take a snapshot
+    for (k, cnt) in copy(dct)  # must take a snapshot
         if haskey(rules, k)
             embed = rules[k]
-            k1 = k[1] * embed
-            k2 = embed * k[2]
-            dct[k1] = get(dct, k1, 0) + cnt
+            k1, k2 = k[1] * embed, embed * k[2]  # derive new pairs
+            dct[k1] = get(dct, k1, 0) + cnt      # increment counters
             dct[k2] = get(dct, k2, 0) + cnt
-            reduced = dct[k] - cnt
-            reduced == 0 && pop!(dct, k)
+            reduced = dct[k] - cnt               # original one must be reduced
+            reduced == 0 && pop!(dct, k)         # do not keep any zeroes
             dct[k] = reduced
         end
     end
-    return dct
 end
 
 function expand(str, seed, rules, steps)
-    seed = copy(seed)
+    counters = copy(seed)  # avoid side effect
     for s in 1:steps
-        step!(seed, rules)
+        step!(counters, rules)
     end
-
     # How to count?
     # 1. The head of the string is special, the first letter is counted once
     # 2. For every pair, count the number of occurrences of the second letter
     dct = Dict{Char,Int}()
     dct[str[1]] = 1
-    for (k, v) in seed
+    for (k, v) in counters
         dct[k[2]] = get(dct, k[2], 0) + v
     end
     return -(reverse(extrema(values(dct)))...)
